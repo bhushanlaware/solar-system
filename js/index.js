db7c86ea2485e16fd78051dbb87bc2fc2ca16817
bhushan laware <bhushanlaware@gmail.com>
2020-07-31 22:40:48 +0530

    First commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $(".container").click(function() {
+    $(".stick").toggleClass(function () {
+      return $(this).is('.open, .close') ? 'open close' : 'open';
+    });
+    $("#menu").toggleClass('showmenu');
+  });
+});
